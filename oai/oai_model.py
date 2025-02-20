19963612236fa459a7dd366de1d611f49e4cd730
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2025-02-20 01:39:28 -0800

    query openAI model with structure for formatting into google docs

--- oai/oai_model.py
+++ oai/oai_model.py
@@ -1,5 +1,40 @@
+from typing import List
 from openai import OpenAI
 import configs.api_keys as config
+from pydantic import BaseModel
 
 
 client = OpenAI(api_key=config.open_ai_api_key)
+PROMPT_FILE = "oai/developer_prompt.txt"
+
+class Subtopic(BaseModel):
+    subtopic_title:str
+    subtopic_body: List[str]
+
+
+class Topic(BaseModel):
+    title:str
+    subtopics:List[Subtopic]
+
+
+class DocumentText(BaseModel):
+    topics:List[Topic]
+
+
+def query_model(question_query):
+    with open(PROMPT_FILE, 'r') as file:  
+        developer_query = file.read()
+    
+    completion = client.beta.chat.completions.parse(
+    model="gpt-4o-2024-08-06",
+    messages = [
+        {"role": "developer", "content": f"{developer_query}"},
+        {
+            "role": "user",
+            "content": f"{question_query}"
+        }
+    ],
+    response_format=DocumentText,
+    )
+    return completion.choices[0].message.parsed
+
