9eeb7b95a31e3bd3e077940be449df46d82e9ef1
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2025-02-23 12:15:19 -0800

    add main workflow code

---
+++ gdocs/write_gdoc.py
@@ -0,0 +1,20 @@
+from gdocs.gdoc_api import get_credentials, create_document, write_doc_heading, write_topic, write_subtopic_title, write_subtopic_body
+from oai.oai_model import DocumentText, Subtopic, Topic, query_text_model, query_whisper
+
+creds = get_credentials()
+
+
+def write_notes():
+    doc_id = create_document(creds, "yepp")
+    write_doc_heading(creds, "test lecture notes", doc_id)
+    lecture_transcript = query_whisper('bla')
+    formatted_notes = query_text_model(lecture_transcript)
+    print(type(formatted_notes), len(formatted_notes.topics))
+    for topic in formatted_notes.topics:
+        write_topic(creds, topic.title, doc_id)
+        for subtopic in topic.subtopics:
+            write_subtopic_title(creds, subtopic.subtopic_title, doc_id)
+            write_subtopic_body(creds, subtopic.subtopic_body, doc_id)
+        print(f'topic {topic.title}, written')
+    print('DONE!!!! ')
+write_notes()
