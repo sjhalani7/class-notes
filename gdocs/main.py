accfc070a44d0d12b89d95fb82d88e77c7a3e7ab
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2025-03-04 14:29:38 -0800

    refactor to main.py

--- gdocs/main.py
+++ gdocs/main.py
@@ -1,5 +1,5 @@
 from gdocs.gdoc_api import get_credentials, create_document, write_doc_heading, write_topic, write_subtopic_title, write_subtopic_body
-from oai.oai_model import DocumentText, Subtopic, Topic, query_text_model, query_whisper
+from oai.oai_model import query_text_model, query_whisper
 from oai.record_audio import record_audio
 import os
 
@@ -7,9 +7,7 @@ creds = get_credentials()
 
 
 
-def write_notes_for_file(lecture_transcript: str, doc_title: str, doc_heading: str=None):
-    doc_id = create_document(creds, doc_title)
-    write_doc_heading(creds, doc_heading if doc_heading else doc_title, doc_id)
+def write_notes_for_file(doc_id: str, lecture_transcript: str):
     formatted_notes = query_text_model(lecture_transcript)
     print(type(formatted_notes), len(formatted_notes.topics))
     for topic in formatted_notes.topics:
@@ -21,12 +19,14 @@ def write_notes_for_file(lecture_transcript: str, doc_title: str, doc_heading: s
     print('DONE!!!! ')
 
 
-def write_all_notes():
-    files = [f for f in os.listdir('audio_files') if os.path.isfile(os.path.join('audio_files', f))]
+def write_all_notes(doc_title: str="Class Notes - Transformers "):
+    files = sorted([f for f in os.listdir('audio_files') if os.path.isfile(os.path.join('audio_files', f))])
+    doc_id = create_document(creds, doc_title)
+    write_doc_heading(creds, doc_title, doc_id)
     for file in files:
         audio_path = f'audio_files/{file}'
         lecture_transcript = query_whisper(audio_path)
-        write_notes_for_file(lecture_transcript, f'Class Notes - {file}')
+        write_notes_for_file(doc_id, lecture_transcript)
         os.remove(audio_path)
 
 def record_class_lecture():
